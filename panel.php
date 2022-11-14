8a1b570077275e1e915fd9f0c9e7cc28750ca64a
Carlos Cereceres <116238118+ccereceres@users.noreply.github.com>
2022-11-13 19:28:05 -0600

    Bootstrap icons. Added logout

--- panel.php
+++ panel.php
@@ -9,20 +9,23 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
             <title>ITParral Panel</title>
             <link rel="stylesheet" href="tpl/css/panel.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
         </head>
         <body class="sb-nav-fixed">
             <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
                 <!-- Navbar Brand-->
                 <a class="navbar-brand ps-3" href="panel.php">English Control</a>
                 <!-- Sidebar Toggle-->
-                <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-                <!-- Navbar Search-->
-                <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0" action="panel.php">
-                    <div class="input-group">
-                        <button class="btn btn-primary" id="btnNavbarSearch" type="submit">Cerrar sesión</button>
-                    </div>
-                </form>
+                <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="bi bi-list" style="font-size: 1.5rem;"></i></i></button>
                 <!-- Navbar-->
+                <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person-gear" style="font-size: 1.5rem;"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="inc/logout.php">Cerrar sesión</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </nav>
             <div id="layoutSidenav">
                 <div id="layoutSidenav_nav">
@@ -73,6 +76,7 @@
                 </div>
             </div>
             <script src="tpl/js/scriptsPanel.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         </body>
         </html>
     <?php
