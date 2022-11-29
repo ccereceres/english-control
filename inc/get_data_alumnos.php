fbb9394f7d041db64c5e27de40ecd76ef8e2678b
Carlos Cereceres <116238118+ccereceres@users.noreply.github.com>
2022-11-28 21:46:36 -0600

    Nueva consulta para obtener alumnos

--- inc/get_data_alumnos.php
+++ inc/get_data_alumnos.php
@@ -2,6 +2,7 @@
 session_start();
 if(isset($_GET['idCurso']) && isset($_GET['accion'])) {
     $idCurso = $_GET['idCurso'];
+    $idProfesor = $_SESSION['id'];
     if ($_GET['accion'] == 'ver') {
         try {
             $conn = new PDO('mysql:host=localhost;dbname=english-control', 'jahy', 'H3nt4i-!#22');
@@ -26,7 +27,12 @@ if(isset($_GET['idCurso']) && isset($_GET['accion'])) {
         } catch (PDOException $exception) {
             die($exception->getMessage());
         }
-        $sql = "SELECT num_control, nombres , apellido_p , apellido_m , calif_1, calif_2, calif_3, calif_4, calif_5 ,calificaciones_id FROM alumnos JOIN cursando c on alumnos.id = c.alumnos_id join curso c2 on c.curso_id = c2.id join carrera c3 on alumnos.carrera_id = c3.id join calificaciones c4 on c.calificaciones_id = c4.id where c2.id = $idCurso";
+        $sql = "SELECT num_control, a.nombres, a.apellido_p, a.apellido_m, calif_1, calif_2, calif_3, calif_4, calif_5, c3.id 
+                FROM profesor JOIN curso c on profesor.id = c.profesor_id 
+                    JOIN cursando c2 on c.id = c2.curso_id 
+                    JOIN alumnos a on a.id = c2.alumnos_id 
+                    JOIN calificaciones c3 on c3.id = c2.calificaciones_id 
+                WHERE profesor.id = $idProfesor AND c.id = $idCurso";
         $st = $conn
             ->query($sql);
         if ($st) {
@@ -44,7 +50,12 @@ if(isset($_GET['idCurso']) && isset($_GET['accion'])) {
         } catch (PDOException $exception) {
             die($exception->getMessage());
         }
-        $sql = "SELECT num_control, nombres , apellido_p , apellido_m , calif_1, calif_2, calif_3, calif_4, calif_5, estado ,calificaciones_id FROM alumnos JOIN cursando c on alumnos.id = c.alumnos_id join curso c2 on c.curso_id = c2.id join carrera c3 on alumnos.carrera_id = c3.id join calificaciones c4 on c.calificaciones_id = c4.id where c2.id = $idCurso";
+        $sql = "SELECT num_control, a.nombres, a.apellido_p, a.apellido_m, calif_1, calif_2, calif_3, calif_4, calif_5, estado, c3.id 
+                FROM profesor JOIN curso c on profesor.id = c.profesor_id 
+                    JOIN cursando c2 on c.id = c2.curso_id 
+                    JOIN alumnos a on a.id = c2.alumnos_id 
+                    JOIN calificaciones c3 on c3.id = c2.calificaciones_id 
+                WHERE profesor.id = $idProfesor AND c.id = $idCurso";
         $st = $conn
             ->query($sql);
         if ($st) {
